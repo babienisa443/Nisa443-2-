27c1f433df6b474dfe82caad68dff3fb252db7ad
Nisachon Yudee <88552638+babienisa443@users.noreply.github.com>
2021-08-13 00:38:57 +0700

    Add files via upload

---
+++ plusminusmultipledivde1..c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+	int a,b;
+	printf("Enter first number :");
+	scanf("%d",&a);
+	printf("Enter second number :");
+	scanf("%d",&b);
+	printf("Result : %d,\t%d,\t%d,\t%d",a+b,a-b,a*b,a/b);
+	
+	return 0;	
+	}
+	
+	
+	
